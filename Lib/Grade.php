f53b35f2e1470e3b3f58b2eda59c9c828adff721
mohuishou <1@lailin.xyz>
2016-08-05 20:44:34 +0800

    【更新】grade

--- Lib/Grade.php
+++ Lib/Grade.php
@@ -13,8 +13,8 @@ class Grade extends JwcBase{
     public function spider()
     {
         //抓取所有及格成绩
-        $url_all_grade='http://202.115.47.141/gradeLnAllAction.do?type=ln&oper=qbinfo&lnxndm';
-        $page=$this->_curl->get($url_all_grade);
+        $url_pass_grade='http://202.115.47.141/gradeLnAllAction.do?type=ln&oper=qbinfo&lnxndm';
+        $page=$this->_curl->get($url_pass_grade);
 
         if ( $this->_curl->error) {
             throw new \Exception('Error: ' .  $this->_curl->errorCode . ': ' .  $this->_curl->errorMessage);
@@ -22,9 +22,12 @@ class Grade extends JwcBase{
         $page=iconv('GBK','UTF-8//IGNORE',$page);
         $page='<!DOCTYPE HTML><html><head></head>'.$page.'</html>';
 
+        //获取学期年份
         $term=QueryList::Query($page,[
             'term'=>['b','text']
         ])->data;
+
+        //获取过了的成绩
         $rule=[
             'courseId'=>['td:eq(0)','text'],
             'lessonId'=>['td:eq(1)','text'],
@@ -34,12 +37,10 @@ class Grade extends JwcBase{
             'courseType'=>['td:eq(5)','text'],
             'grade'=>['td:eq(6)','text'],
         ];
-
-
         $pass=QueryList::Query($page,$rule,'#user tr')->data;
 
-        /*--------------数据整理---------------*/
-        $b=[];
+        //数据整理
+        $grade_pass=[];
         $i=-1;
         foreach($pass as $k => $v){
             if(empty($v['courseId'])){
@@ -50,27 +51,23 @@ class Grade extends JwcBase{
             $v['term']=$term[$i]['term'];
             $b[$i][]=$v;
         }
+        $grade=$grade_pass;
 
-        $gradeData=$b;
-
-        #抓取当前不及格成绩，并将当前不及格成绩插入本学期
-
-        $url2='http://202.115.47.141/gradeLnAllAction.do?type=ln&oper=bjg';
-        $page=$this->_curl->get($url2);
+        //抓取当前不及格成绩，并将当前不及格成绩插入本学期
+        $url_fail_grade='http://202.115.47.141/gradeLnAllAction.do?type=ln&oper=bjg';
+        $page=$this->_curl->get($url_fail_grade);
         if ( $this->_curl->error) {
             throw new \Exception('Error: ' .  $this->_curl->errorCode . ': ' .  $this->_curl->errorMessage);
         }
-        $loss=QueryList::Query($page,$rule,'#user:eq(0) tr',"UTF-8")->data;
-        foreach($loss as $k => $v){
+        $fail=QueryList::Query($page,$rule,'#user:eq(0) tr',"UTF-8")->data;
+        foreach($fail as $k => $v){
             if(!empty($v['courseId'])){
                 $v['term']=$term[$i]['term'];
                 $v['termId']=$i+1;
-                $gradeData[$i]=$v;
+                $grade[$i]=$v;
             }
         }
 
-
-
-        return $gradeData;
+        return $grade;
     }
 }
