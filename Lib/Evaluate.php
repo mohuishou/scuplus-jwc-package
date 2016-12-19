b2b42ffd54b1b8218fbdccadac2eef5e0b6b0ea5
mohuishou <1@lailin.xyz>
2016-12-19 12:09:58 +0800

    update

--- Lib/Evaluate.php
+++ Lib/Evaluate.php
@@ -55,13 +55,29 @@ class Evaluate extends JwcBase{
     }
 
     /**
-     * 获取评教信息
+     * 获取已经评教的信息
      * @author mohuishou<1@lailin.xyz>
      * @param int $page 页码
      * @return mixed
      */
-    public function getEvaluate($page=1){
-        return $this->login()->spider($page);
+    public function getEvaluateData($params){
+        if($params['status']!=1){
+            return false;
+        }
+        //登录教务处
+        $this->login();
+        $post_params=$this->evaluateFormParams($params);
+        $post_params['oper']='wjResultShow';
+        $formUrl="http://202.115.47.141/jxpgXsAction.do";//评教表单页面
+        //抓取表单页面
+        $url=$formUrl.'?'.http_build_query($post_params);
+        $html=$this->get($url);
+//        return $html;
+        $rules=[
+           'comment'=>['textarea[name="zgpj"]','text']
+        ];
+        $res=QueryList::Query($html,$rules)->data;
+        return $res;
     }
 
     /**
@@ -73,7 +89,7 @@ class Evaluate extends JwcBase{
      */
     public function evaluate($params){
         //参数检测
-        $param_verify=['jwc_evaluate_id','jwc_teacher_id','teacher_name','teacher_type','course_name','course_id','verify','star','comment'];
+        $param_verify=['jwc_evaluate_id','jwc_teacher_id','teacher_name','teacher_type','course_name','course_id','verify_name','verify_value','star','comment'];
         foreach ($param_verify as $v){
             if(!array_key_exists($v, $params)){
                 throw new \Exception('参数错误！'.$v.'必须','4221');
@@ -84,7 +100,7 @@ class Evaluate extends JwcBase{
         $this->login();
 
         //构造评教参数
-        $params_data=$this->evaluateData($params);
+        $params_data=$this->evaluateParams($params);
 
         //提交
         $url='http://202.115.47.141/jxpgXsAction.do?'.http_build_query($params_data);
@@ -116,33 +132,12 @@ class Evaluate extends JwcBase{
      * @param $params
      * @return array
      */
-    protected function evaluateData($params){
+    protected function evaluateParams($params){
         $params_data=[];
 
-        //问卷列表页面
-        $url="http://202.115.47.141/jxpgXsAction.do?oper=listWj&pageSize=60";
-        $this->get($url);
 
-        //首先抓取表单页面，构造需要评教的参数
+        $post_params=$this->evaluateFormParams($params);
         $formUrl="http://202.115.47.141/jxpgXsAction.do";//评教表单页面
-        $post_params['wjbm']=$params['jwc_evaluate_id']; //评教id
-        $post_params['bpr']=$params['jwc_teacher_id']; //教师id
-        $post_params['pgnr']=$params['course_id']; //课程id
-
-        //固定参数
-        $post_params['oper']='wjShow'; //固定参数
-        $post_params['pageSize']='20'; //固定参数
-        $post_params['page']='1'; //固定参数
-        $post_params['currentPage']='1'; //固定参数
-        $post_params['pageNo']=''; //固定参数
-
-        //中文需要先转换为GBK
-        $post_params['wjmc']=$this->utf82gbk($params['evaluate_type']); //评教类型
-        $post_params['bprm']=$this->utf82gbk($params['teacher_name']); //教师名
-        $post_params['pgnrm']=$this->utf82gbk($params['course_name']); //课程名
-
-        $post_params[$params['verify']['name']]=$params['verify']['value']; //应该是验证参数
-
         //抓取表单页面
         $url=$formUrl.'?'.http_build_query($post_params);
         $html=$this->get($url);
@@ -172,6 +167,34 @@ class Evaluate extends JwcBase{
         $params_data['zgpj']=$this->utf82gbk($params['comment']);
         $params_data['oper']='wjpg';
         return $params_data;
+    }
+
+    protected function evaluateFormParams($params){
+        //问卷列表页面
+        $url="http://202.115.47.141/jxpgXsAction.do?oper=listWj&pageSize=60";
+        $this->get($url);
+
+        //首先抓取表单页面，构造需要评教的参数
+        $post_params['wjbm']=$params['jwc_evaluate_id']; //评教id
+        $post_params['bpr']=$params['jwc_teacher_id']; //教师id
+        $post_params['pgnr']=$params['course_id']; //课程id
+
+        //固定参数
+        $post_params['oper']='wjShow'; //固定参数
+        $post_params['pageSize']='20'; //固定参数
+        $post_params['page']='1'; //固定参数
+        $post_params['currentPage']='1'; //固定参数
+        $post_params['pageNo']=''; //固定参数
+
+        //中文需要先转换为GBK
+        $post_params['wjmc']=$this->utf82gbk($params['evaluate_type']); //评教类型
+        $post_params['bprm']=$this->utf82gbk($params['teacher_name']); //教师名
+        $post_params['pgnrm']=$this->utf82gbk($params['course_name']); //课程名
+
+        $post_params[$params['verify_name']]=$params['verify_value']; //应该是验证参数
+        return $post_params;
+
+
     }
 
     protected function utf82gbk($str){
